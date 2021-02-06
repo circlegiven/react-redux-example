da36660df2bf282b19c2c9a4615eeee1fcc1a4c9
circlegiven <circlegiven@gmail.com>
2021-02-06 23:16:51 +0900

    Use middleware

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import reducer from './store';
 
-const store = createStore(reducer);
+// 인터셉터
+const interceptorReduxAction = store => next => action =>{
+    // 캐치할 action
+    console.log(action.type);
+
+    return next(action);
+}
+
+const store = createStore(
+    reducer,
+    applyMiddleware(interceptorReduxAction)
+);
+
 
 ReactDOM.render(
   <Provider store={store}>
